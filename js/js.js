d56748ddf1c97773fe32463a09161bb72e8f0d01
Maro de Melo <marodemelo10@gmail.com>
2021-07-24 21:53:26 -0300

    inserida colisao entre amigo e carro

--- js/js.js
+++ js/js.js
@@ -232,8 +232,21 @@ function start() { // Inicio da fun��o start()
 
             reposicionaAmigo();
             $("#amigo").remove();
-        }    
+        } 
+
+        if (colisao6.length>0) {
+	    
+            amigoX = parseInt($("#amigo").css("left"));
+            amigoY = parseInt($("#amigo").css("top"));
+            explosao3(amigoX,amigoY);
+            $("#amigo").remove();
+                    
+            reposicionaAmigo();
+                    
+        }
     }
+
+    
 
 
         function explosao1(inimigo1X,inimigo1Y) {
@@ -276,6 +289,20 @@ function start() { // Inicio da fun��o start()
                 
                 
         }
+
+        function explosao3(amigoX,amigoY) {
+            $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+            $("#explosao3").css("top",amigoY);
+            $("#explosao3").css("left",amigoX);
+            var tempoExplosao3=window.setInterval(resetaExplosao3, 1000);
+            function resetaExplosao3() {
+            $("#explosao3").remove();
+            window.clearInterval(tempoExplosao3);
+            tempoExplosao3=null;
+                    
+            }
+            
+            }
 
             function reposicionaInimigo2() {
 	
